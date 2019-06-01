6df71410ce3200a4f5a18c756771313b96140a93
Brandon Nichols <brandonaronnichols@gamil.com>
2019-05-31 20:36:48 -0500

    Finished dinosaur sectiion of Objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -28,23 +28,34 @@ const stegosaurus = {
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 
+const velociraptor = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous'
+};
+
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+tyrannosaurus.roar = function(){
+  return 'RAWERSRARARWERSARARARRRR!'
+};
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
