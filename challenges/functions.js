b3d37e943028e7d3e6e95ccf50cb5d7ae1b319d1
Brandon Nichols <brandonaronnichols@gamil.com>
2019-05-31 20:21:57 -0500

    Finsh step 1 fo functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(val, arg, cb){
+  return cb(val, arg);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
