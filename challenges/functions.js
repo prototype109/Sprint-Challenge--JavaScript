92ec2d752c66b40a7d2b1d0a198b53ea8f68c5ab
Brandon Nichols <brandonaronnichols@gamil.com>
2019-05-31 20:24:27 -0500

    Finished step 2 of functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,8 +7,8 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
-function consume(val, arg, cb){
-  return cb(val, arg);
+function consume(arg1, arg2, cb){
+  return cb(arg1, arg2);
 }
 
 
@@ -18,6 +18,17 @@ function consume(val, arg, cb){
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2){
+  return num1 + num2;
+}
+
+function multiply(num1, num2){
+  return num1 * num2;
+}
+
+function greeting(firstName, lastName){
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
